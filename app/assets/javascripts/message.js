c217d10f5dda9c52d9b52fab7966af2dd246cfba
ishiyama2000 <sakananohonee@gmail.com>
2020-06-18 22:05:27 +0900

    チャット画面の自動更新機能の実装（2回目）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -75,13 +75,13 @@ $('#new_message').on('submit', function(e){
   })
   .done(function(messages) {
     if (messages.length !== 0) {
-    var insertHTML = '';
-    $.each(messages, function(i, message) {
-      insertHTML += buildHTML(message)
-    });
-    $('.messages').append(insertHTML);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
   })
   .fail(function() {
     alert('error');
