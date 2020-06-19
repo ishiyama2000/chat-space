21a1f451e3f2dee4c6ad6505f7d73815be1aa0e9
ishiyama2000 <sakananohonee@gmail.com>
2020-06-19 19:54:58 +0900

    steep2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,18 @@
+# encoding: utf-8
+
 class ImageUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  # storage :fog
-# 〜省略〜
   process resize_to_fit: [800, 800]
-# 〜省略〜
+
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
